60764286a1dbb63a0f959a53453eb0d674c3d842
Duncan Moyo <duncanfmoyo@gmail.com>
2020-07-21 10:19:51 +0200

    initial commit

---
+++ src/PortfolioData.js
@@ -0,0 +1,51 @@
+const PortfolioData = {
+  name: 'Duncan Moyo',
+  role: 'Frontend Developer',
+  roleDescription: "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/DuncanMoyo',
+      className: 'fa fa-github'
+    },
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/duncanfm',
+      className: 'fa fa-facebook-official'
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/duncanfm',
+      className: 'fa fa-twitter'
+    }
+  ],
+  aboutMe: 'I am a self-taught Web Developer, currently diving deeper into Javascript through React with the intention of learning and applying more. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.',
+  skills: [
+    {
+      skillName: 'HTML5'
+    },
+    {
+      skillName: 'CSS'
+    },
+    {
+      skillName: 'React.js'
+    },
+    {
+      skillName: 'Python'
+    }
+  ],
+  portfolio: [
+    {
+      name: 'project1',
+      description: 'App',
+      image: 'jpg file'
+    },
+    {
+      name: 'project2',
+      description: 'App',
+      image: 'jpg file'
+    },
+  ]
+}
+
+export default PortfolioData
