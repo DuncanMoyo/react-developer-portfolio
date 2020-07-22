d87018ded421591d5f2fdc8f252d4084b195beee
Duncan Moyo <duncanfmoyo@gmail.com>
2020-07-22 14:56:39 +0200

    about section rendering data

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -1,53 +1,59 @@
 const portfolioData = {
-  name: 'Duncan Moyo',
-  role: 'Frontend Developer',
-  email: 'duncanfmoyo@gmail.com',
-  phoneNumber: '+27 67 073 7874',
-  roleDescription: "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  name: "Duncan Moyo",
+  role: "Frontend Developer",
+  email: "duncanfmoyo@gmail.com",
+  phoneNumber: "+27 67 073 7874",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
   socialLinks: [
     {
-      name: 'GitHub',
-      url: 'https://github.com/DuncanMoyo',
-      className: 'fa fa-github'
+      name: "GitHub",
+      url: "https://github.com/DuncanMoyo",
+      className: "fa fa-github",
     },
     {
-      name: 'Facebook',
-      url: 'https://www.facebook.com/duncanfm',
-      className: 'fa fa-facebook-official'
+      name: "Facebook",
+      url: "https://www.facebook.com/duncanfm",
+      className: "fa fa-facebook-official",
     },
     {
-      name: 'Twitter',
-      url: 'https://twitter.com/duncanfm',
-      className: 'fa fa-twitter'
-    }
+      name: "Twitter",
+      url: "https://twitter.com/duncanfm",
+      className: "fa fa-twitter",
+    },
   ],
-  aboutMe: 'I am a self-taught Web Developer, currently diving deeper into Javascript through React with the intention of learning and applying more. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.',
+  aboutMe:
+    "I am a self-taught Web Developer, currently diving deeper into Javascript through React with the intention of learning and applying more. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
   skills: [
     {
-      skillName: 'HTML5'
+      skillName: "HTML5",
+      skillLevel: 85,
     },
     {
-      skillName: 'CSS'
+      skillName: "CSS",
+      skillLevel: 70
     },
     {
-      skillName: 'React.js'
+      skillName: "React.js",
+      skillLevel: 75
     },
     {
-      skillName: 'Python'
-    }
+      skillName: "Python",
+      skillLevel: 66
+    },
   ],
   portfolio: [
     {
-      name: 'project1',
-      description: 'App',
-      image: 'jpg file'
+      name: "project1",
+      description: "App",
+      image: "jpg file",
     },
     {
-      name: 'project2',
-      description: 'App',
-      image: 'jpg file'
+      name: "project2",
+      description: "App",
+      image: "jpg file",
     },
-  ]
-}
+  ],
+};
 
-export default PortfolioData
+export default portfolioData;
--- src/components/About.js
+++ src/components/About.js
@@ -1,63 +1,102 @@
-import React, { Component } from 'react';
-import classes from './css/Style.module.css';
+import React, { Component } from "react";
+import classes from "./css/Style.module.css";
+import portfolioData from "../PortfolioData";
+import {Progress} from 'reactstrap';
 
 class About extends Component {
-  
   render() {
-    let aboutData = this.props.portfolioData
     return (
-      <section id="about" className="about-mf sect-pt4 route">
+      <section
+        id="about"
+        className={(classes.AboutMf, classes.SectPt4)}
+        route="true"
+      >
         <div className="container">
           <div className="row">
             <div className="col-sm-12">
-              <div className="box-shadow-full">
+              <div className={classes.BoxShadowFull}>
                 <div className="row">
                   <div className="col-md-6">
                     <div className="row">
                       <div className="col-sm-6 col-md-5">
                         <div className="about-img">
-                          <img src="assets/img/testimonial-2.jpg" className="img-fluid rounded b-shadow-a" alt=""/>
+                          <img
+                            src="img/testimonial-2.jpg"
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
                         </div>
                       </div>
                       <div className="col-sm-6 col-md-7">
                         <div className="about-info">
-                          <p><span className="title-s">Name: </span> <span>Morgan Freeman</span></p>
-                          <p><span className="title-s">Profile: </span> <span>full stack developer</span></p>
-                          <p><span className="title-s">Email: </span> <span>contact@example.com</span></p>
-                          <p><span className="title-s">Phone: </span> <span>(617) 557-0089</span></p>
+                          <p>
+                            <span className={classes.TitleS}>Name: </span>{" "}
+                            <span>{portfolioData.name}</span>
+                          </p>
+                          <p>
+                            <span className={classes.TitleS}>Profile: </span>{" "}
+                            <span>{portfolioData.role}</span>
+                          </p>
+                          <p>
+                            <span className={classes.TitleS}>Email: </span>{" "}
+                            <span>{portfolioData.email}</span>
+                          </p>
+                          <p>
+                            <span className={classes.TitleS}>Phone: </span>{" "}
+                            <span>{portfolioData.phoneNumber}</span>
+                          </p>
                         </div>
                       </div>
                     </div>
-                    <div className="skill-mf">
-                      <p className="title-s">Skill</p>
-                      <span>HTML</span> <span className="pull-right">85%</span>
-                      <div className="progress">
-                        <div className="progress-bar" role="progressbar" style={{width: '85%', ariaValuenow: "85", ariaValuemin:"0" ,ariaValuemax:"100"}}></div>
-                      </div>
+                    <div className={classes.SkillMf}>
+                      <p className={classes.TitleS}>Skill</p>
+                      {portfolioData.skills.map((skill) => {
+                        return (
+                          <div>
+                            <span><strong>{skill.skillName}</strong></span>{" "}
+                            <strong><span style={{float: 'right'}}>{skill.skillLevel}%</span></strong>
+                            
+                            <div className={classes.Progress}>
+                              
+                              <div
+                                className={classes.ProgressBar}
+                                role="progressbar"
+                                style={{
+                                  width: "85%",
+                                  ariaValuenow: "85",
+                                  ariaValuemin: "0",
+                                  ariaValuemax: "100",
+                                }}
+                              ></div>
+                            </div>
+                          </div>
+                        );
+                      })}
                     </div>
                   </div>
                   <div className="col-md-6">
                     <div className="about-me pt-4 pt-md-0">
-                      <div className="title-box-2">
-                        <h5 className="title-left">
-                          About me
-                        </h5>
+                      <div className={classes.TitleBox2}>
+                        <h5 className={classes.TitleLeft}>About me</h5>
                       </div>
                       <p className="lead">
-                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id
-                        imperdiet et, porttitor
-                        at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla
-                        porttitor accumsan tincidunt.
+                        Curabitur non nulla sit amet nisl tempus convallis quis
+                        ac lectus. Curabitur arcu erat, accumsan id imperdiet
+                        et, porttitor at sem. Praesent sapien massa, convallis a
+                        pellentesque nec, egestas non nisi. Nulla porttitor
+                        accumsan tincidunt.
                       </p>
                       <p className="lead">
-                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus suscipit tortor eget felis
-                        porttitor volutpat. Vestibulum
-                        ac diam sit amet quam vehicula elementum sed sit amet dui. porttitor at sem.
+                        Mauris blandit aliquet elit, eget tincidunt nibh
+                        pulvinar a. Vivamus suscipit tortor eget felis porttitor
+                        volutpat. Vestibulum ac diam sit amet quam vehicula
+                        elementum sed sit amet dui. porttitor at sem.
                       </p>
                       <p className="lead">
-                        Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
-                        Nulla porttitor accumsan
-                        tincidunt. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Nulla porttitor accumsan tincidunt. Quisque velit nisi,
+                        pretium ut lacinia in, elementum id enim. Nulla
+                        porttitor accumsan tincidunt. Mauris blandit aliquet
+                        elit, eget tincidunt nibh pulvinar a.
                       </p>
                     </div>
                   </div>
@@ -67,8 +106,8 @@ class About extends Component {
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default About
+export default About;
